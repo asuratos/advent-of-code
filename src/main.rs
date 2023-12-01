3626b706ae460dd3457c73ed873bd8bb3519fddf
Aldrich Suratos <cyurio@gmail.com>
2023-12-01 16:49:47 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,13 @@ fn main() -> Result<(), String> {
     let input = File::open("input").map_err(|_| "Couldn't read file")?;
     let reader = BufReader::new(input);
 
-    println!("{:?}",
+    println!(
+        "{:?}",
         reader
-        .lines()
-        .map(|line| get_first_and_last_numbers(&line.map_err(|_| "Reader sent a wrong line")?))
-        .sum::<Result<i32, String>>());
+            .lines()
+            .map(|line| get_first_and_last_numbers(&line.map_err(|_| "Reader sent a wrong line")?))
+            .sum::<Result<i32, String>>()
+    );
 
     Ok(())
 }
