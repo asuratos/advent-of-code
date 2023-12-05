de2d21c3be6960e83ad2b6b3234e968a959372e1
Aldrich Suratos <cyurio@gmail.com>
2023-12-05 17:19:34 +0900

    a bit of work on day3

--- 2023/day-03/src/main.rs
+++ 2023/day-03/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(PartialEq, Eq, Hash)]
+struct Location(isize, isize);
+
+impl Location {
+    fn get_neighbors(&self) -> HashSet<Location> {
+        let mut neighbors: HashSet<Location> = HashSet::new();
+
+        [-1, 0, 1].into_iter().for_each(|xdiff| {
+            [-1, 0, 1].into_iter().for_each(|ydiff| {
+                neighbors.insert(Location(self.0 + xdiff, self.1 + ydiff));
+            })
+        });
+
+        neighbors
+    }
+}
+
+/// struct to hold the schematic, for easier addressing of specific locations.
+/// positive X = right, positive Y = down
+struct Schematic {
+    contents: Vec<String>,
+}
+
+impl Schematic {
+    fn read_file(path: &str) -> Result<Schematic, String> {
+        let input = File::open(path).map_err(|_| "Couldn't open file")?;
+        let reader = BufReader::new(input);
+
+        let schem: Vec<String> = reader
+            .lines()
+            .map(|line| line.expect("Line could not be read"))
+            .collect();
+
+        Ok(Schematic { contents: schem })
+    }
+    fn symbol_at(&self, loc: Location) -> Option<char> {
+        self.contents[loc.1].chars().nth(loc.0)
+    }
+}
+
+fn main() -> Result<(), String> {
+    let schem = Schematic::read_file("input.txt")?;
+
+    Ok(())
 }
