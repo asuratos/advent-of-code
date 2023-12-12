f42213d4d368ba2fd6a98dc4ea0b865bb82b5d5b
Aldrich Suratos <cyurio@gmail.com>
2023-12-12 16:09:38 +0900

    day6-2 done

--- 2023/day-06/src/main.rs
+++ 2023/day-06/src/main.rs
@@ -1,20 +1,22 @@
-fn distance_travelled(hold: &i32, racetime: &i32) -> i32 {
+fn distance_travelled(hold: &i64, racetime: &i64) -> i64{
     hold * (racetime - hold)
 }
 
-fn count_wins(racetime: i32, record: i32) -> i32 {
+fn count_wins(racetime: i64, record: i64) -> i64 {
     (0..racetime as usize)
-        .filter(|&hold| distance_travelled(&(hold as i32), &racetime) > record)
-        .count() as i32
+        .filter(|&hold| distance_travelled(&(hold as i64), &racetime) > record)
+        .count() as i64
 }
 
 fn main() {
-    const GAMES: [(i32, i32); 4] = [(58, 434), (81, 1041), (96, 2219), (76, 1218)];
+    const GAMES: [(i64, i64); 4] = [(58, 434), (81, 1041), (96, 2219), (76, 1218)];
 
     let product = GAMES
         .iter()
         .map(|&(racetime, record)| count_wins(racetime, record))
-        .product::<i32>();
+        .product::<i64>();
 
     println!("{}", product);
+
+    println!("{}", count_wins( 58819676, 434104122191218));
 }
