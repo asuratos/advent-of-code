2ae6bdbeb5cbc03b95b44b520774ab206d0fe813
Aldrich Suratos <cyurio@gmail.com>
2023-12-12 16:05:06 +0900

    Day6-1 done

--- 2023/day-06/src/main.rs
+++ 2023/day-06/src/main.rs
@@ -1,3 +1,20 @@
+fn distance_travelled(hold: &i32, racetime: &i32) -> i32 {
+    hold * (racetime - hold)
+}
+
+fn count_wins(racetime: i32, record: i32) -> i32 {
+    (0..racetime as usize)
+        .filter(|&hold| distance_travelled(&(hold as i32), &racetime) > record)
+        .count() as i32
+}
+
 fn main() {
-    println!("Hello, world!");
+    const GAMES: [(i32, i32); 4] = [(58, 434), (81, 1041), (96, 2219), (76, 1218)];
+
+    let product = GAMES
+        .iter()
+        .map(|&(racetime, record)| count_wins(racetime, record))
+        .product::<i32>();
+
+    println!("{}", product);
 }
